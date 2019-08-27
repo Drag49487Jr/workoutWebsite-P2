47995f24eacdd64cbc9dfe8fcf93d9f14bc8e766
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-08-27 12:23:54 -0500

    update exercise module

--- models/regime.js
+++ models/regime.js
@@ -12,9 +12,23 @@ var mealPlanSchema = new Schema ({
 
 
 var exerciseSchema = new Schema ({
-    name:String,
-    sets:Number,
-    reps:Number,
+    name:{
+        type:String,
+        enum: ['Squats w/ Weight', 'Barbell Hip Thrust', 'Dumbbell Bulgarian Split', 'Heals to the Heavans',
+        'Alternating Thread the Needle', 'Stationary Bike'],
+    },
+    sets:{
+        type:Number,
+        enum:['1', '2', '3', '4'],
+    },
+    reps:{
+        type:Number,
+        enum:['0','10','15','20'],
+    },
+    time: {
+        type:Number,
+        enum:['30sec', '45sec', '1min', '1min30sec', '2min'],
+    }
 });
 
 var regimentSchema = new Schema ({
